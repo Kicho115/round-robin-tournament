2e6a17ccdc78eeeccb6de917c98ba6697bd2968f
Rafael <rafael.negrete@cetys.edu.mx>
2025-10-17 00:40:57 -0700

    Cambios necesarios

---
+++ set_env.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+set -e
+
+export VCPKG_ROOT="$HOME/vcpkg"   # ajusta si lo tienes en otra ruta
+export CMAKE_TOOLCHAIN_FILE="$VCPKG_ROOT/scripts/buildsystems/vcpkg.cmake"
+export VCPKG_DEFAULT_TRIPLET="x64-linux"
+export VCPKG_FEATURE_FLAGS="manifests,binarycaching"
+
+echo "VCPKG_ROOT=$VCPKG_ROOT"
+echo "CMAKE_TOOLCHAIN_FILE=$CMAKE_TOOLCHAIN_FILE"
+echo "VCPKG_DEFAULT_TRIPLET=$VCPKG_DEFAULT_TRIPLET"
+echo "VCPKG_FEATURE_FLAGS=$VCPKG_FEATURE_FLAGS"
+
+# Comandos a correr para configurar paqueterias en computadora de Rafita (Falta por corroborar que esto funcione en otros equipos)
+# rm -rf cmake-build-debug CMakeCache.txt
+# source ./setup_env.sh
+#
+# cmake -S . -B cmake-build-debug -G Ninja \
+#   -DCMAKE_TOOLCHAIN_FILE="$CMAKE_TOOLCHAIN_FILE" \
+#   -DVCPKG_TARGET_TRIPLET="$VCPKG_DEFAULT_TRIPLET"
+#
+# cmake -S . -B cmake-build-debug (Este comando es muy parecido al del bloque pasado, pero sirve como confirmacion)
+# cmake --build cmake-build-debug --target tournament_tests_runner
+# ./cmake-build-debug/tournament_services/tests/tournament_tests_runner
